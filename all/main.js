0110f3b3d39a410f79249009f1fad65a96787b08
titancodes7 <ivdmerwe05126@gmail.com>
2024-09-03 14:33:18 +0200

    push 8

---
+++ all/main.js
@@ -0,0 +1,10 @@
+﻿const whatsappIcon = document.querySelector('.whatsapp-chat img');
+const whatsappPopup = document.getElementById('whatsappPopup');
+
+whatsappIcon.addEventListener('click', function () {
+    whatsappPopup.style.display = 'block';
+});
+
+function closeChat() {
+    whatsappPopup.style.display = 'none';
+}
